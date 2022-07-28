c322feba453ef26ff4274ab107e84285432ddcc9
ha-manel <manelhammouche3@gmail.com>
2022-07-28 11:34:20 +0100

    Add fizzbuzz function

--- solver.rb
+++ solver.rb
@@ -1,13 +1,19 @@
-class Solver 
+class Solver
+  def factorial(num)
+    return 1 if num.zero?
+    raise 'Please put a positive integer' if num.negative?
 
-    def factorial(n)
-        return 1 if n == 0
-        raise 'Please put a positive integer' if n < 0
-        return n * factorial(n - 1)
-    end
+    num * factorial(num - 1)
+  end
 
-    def reverse(str)
-        str.reverse
-    end
+  def reverse(str)
+    str.reverse
+  end
+
+  def fizzbuzz(num)
+    return 'fizzbuzz' if (num % 3).zero? && (num % 5).zero?
+    return 'fizz' if (num % 3).zero?
+    return 'buzz' if (num % 5).zero?
+    return num.to_s unless (num % 3).zero? && (num % 5).zero?
+  end
 end
-

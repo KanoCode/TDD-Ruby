2641f098f0c0dd03001f4b622ed275b9f5c412e8
ha-manel <manelhammouche3@gmail.com>
2022-07-28 11:10:49 +0100

    Add reverse and fizzbuzz tests

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -13,5 +13,21 @@ describe Solver do
   it 'Should return 1' do
     expect(@solver.factorial(0)).should eq 1    
   end
+
+  it "should return 'olleh'" do
+    expect(@solver.reverse('hello')).should eql 'olleh' 
+  end
+
+  it 'should return fizz' do
+    expect(@solver.fizzbuzz(9)).should eql 'fizz'
+  end
+
+  it 'should return fizzbuzz' do
+    expect(@solver.fizzbuzz(15)).should eql 'fizzbuzz'
+  end
+
+  it 'should return 7' do
+    expect(@solver.fizzbuzz(7)).should eql '7'
+  end
 end
 
